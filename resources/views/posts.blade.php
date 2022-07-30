4cb742f0ba3e6ffccad9a6361a41dd14cf59e44f
ishfaq ahmed <ahmedishfaq737@gmail.com>
2022-07-30 17:58:19 +0600

    Created a basic blog plage with basic routing in laravel .

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<link rel="stylesheet" href="/app.css">
+<body>
+    <article>        
+    <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil deserunt animi voluptatibus! Quo, facilis. Ipsum blanditiis provident nemo illo saepe nisi modi id in quos repellendus eveniet at veritatis, minima voluptas, earum amet ad aliquid quisquam explicabo. Eaque iure mollitia dolor ipsum eligendi saepe assumenda illum? Blanditiis ad hic illo.</p>    
+    </article>
+    <article>
+    <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil deserunt animi voluptatibus! Quo, facilis. Ipsum blanditiis provident nemo illo saepe nisi modi id in quos repellendus eveniet at veritatis, minima voluptas, earum amet ad aliquid quisquam explicabo. Eaque iure mollitia dolor ipsum eligendi saepe assumenda illum? Blanditiis ad hic illo.</p>    
+    </article>
+    <article>
+    <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil deserunt animi voluptatibus! Quo, facilis. Ipsum blanditiis provident nemo illo saepe nisi modi id in quos repellendus eveniet at veritatis, minima voluptas, earum amet ad aliquid quisquam explicabo. Eaque iure mollitia dolor ipsum eligendi saepe assumenda illum? Blanditiis ad hic illo.</p>    
+    </article>
+</body>
+</html>
