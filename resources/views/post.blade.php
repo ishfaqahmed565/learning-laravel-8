44194860bd491bd48f10a47708a420e4ae092cad
ishfaq ahmed <ahmedishfaq737@gmail.com>
2022-08-02 05:40:06 +0600

    Implemented basic blade syntax

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,8 +9,10 @@
 <body>
     
             <article>
-                <h1><?=$post->title?></h1>
-                <div><?=$post->body?></div>
+                <h1>
+                {{$post->title}}</h1>
+                </h1>
+                <div>{!!$post->body!!}</div>
             </article>
         <a href="/">Back...</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,13 +9,12 @@
 <link rel="stylesheet" href="/app.css">
 <body>
     
-        <?php foreach($posts as $post): ?>
-           
+        @foreach($posts as $post)  
             <article>
-                <h1><a href="posts/<?=$post->slug?>"><?=$post->title?></a></h1>
-                <div><?=$post->excerpt?></div>
+                <h1><a href="posts/{{$post->slug}}">{{$post->title}}</a></h1>
+                <div>{{$post->excerpt}}</div>
             </article>
-        <?php endforeach; ?>
+        @endforeach
     
 </body>
 </html>
